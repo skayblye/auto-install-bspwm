f229deb078d6dfaacdf36b5a8acf27725bbafcf8
skayblye <santosyahir3@gmail.com>
2022-08-14 15:53:48 -0600

    first commit

---
+++ install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import subprocess
+
+#instalacion de funestes y instalcion de terminal 
+def installfont():
+    
+    #comandos
+    font = "sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O /usr/local/share/fonts/Hack.zip"
+    zipfont = "sudo unzip /usr/local/share/fonts/Hack.zip -d /usr/local/share/fonts/"
+    deletezip = "sudo rm /usr/local/share/fonts/Hack.zip"
+    
+    subprocess.run(font, zipfont, deletezip, shell=True)    
+    
+
+installfont()
