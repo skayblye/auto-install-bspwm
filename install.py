cd45a256350a88102d80718394730e1ef2a347e0
skayblye <santosyahir3@gmail.com>
2022-08-16 10:50:07 -0600

    edicion de errores en la funcion installfont

--- install.py
+++ install.py
@@ -6,13 +6,15 @@ import subprocess
 def installfont():
     
     #comandos
-    font = "sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O /usr/local/share/fonts/Hack.zip"
-    zipfont = "sudo unzip /usr/local/share/fonts/Hack.zip -d /usr/local/share/fonts/"
-    deletezip = "sudo rm /usr/local/share/fonts/Hack.zip"
+    #font = "sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O /usr/local/share/fonts/Hack.zip"
+    #zipfont = "sudo unzip /usr/local/share/fonts/Hack.zip -d /usr/local/share/fonts/"
+    #deletezip = "sudo rm /usr/local/share/fonts/Hack.zip"
     
-    subprocess.run(font, zipfont, deletezip, shell=True)    
+    installhackfonts = ["sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O /usr/local/share/fonts/Hack.zip",
+                        "sudo unzip /usr/local/share/fonts/Hack.zip -d /usr/local/share/fonts/",
+                        "sudo rm /usr/local/share/fonts/Hack.zip"]
+    
+    subprocess.run(installhackfonts, shell=True)    
     
 
 installfont()
-
-echo "hola"
